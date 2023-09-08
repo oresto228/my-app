8334022f83d295e73e71eee07b3a107ec5b22c17
Orest <orest361@gmail.com>
2023-09-08 16:13:37 +0300

    footer,hoverable underline,adaptation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppPageComponent } from './components/app-page/app-page.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     HeaderComponent,
-    AppPageComponent
+    AppPageComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
